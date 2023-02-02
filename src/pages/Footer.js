25b84f08239279fd0cd5e3a6e8ade854f146b190
yavetisyan <mr.avetisyan@yahoo.com>
2023-02-02 23:28:57 +0400

    fix active nav links

--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -1,23 +1,22 @@
-import React from 'react'
-import {TbSwimming} from "react-icons/tb"
-import {ImFacebook} from "react-icons/im"
-import {Link} from 'react-router-dom'
-import {AiFillInstagram} from 'react-icons/ai'
-import {navlinks, roolsLink} from "../utilits/navlinks";
-import Map from '../componets/Map'
-
+import React from "react";
+import { TbSwimming } from "react-icons/tb";
+import { ImFacebook } from "react-icons/im";
+import { Link, NavLink } from "react-router-dom";
+import { AiFillInstagram } from "react-icons/ai";
+import { navlinks, roolsLink } from "../utilits/navlinks";
+import Map from "../componets/Map";
 
 const Footer = () => {
   return (
-    <div className='footer'>
+    <div className="footer">
       <div className="container">
-        <div className='footer_title'>
-          <div className='footer_inner'>
+        <div className="footer_title">
+          <div className="footer_inner">
             <div className="footer_inner-logo">
               <div className="footerLogo">
-                <Link to='/' className='logo'>
-                  <div className='footerLogo_title'>
-                    <TbSwimming className="icon"/>
+                <Link to="/" className="logo">
+                  <div className="footerLogo_title">
+                    <TbSwimming className="icon" />
                     <div>
                       <h4>SCHWIMMSCHULE</h4>
                       <h4>swimming from zero to hero</h4>
@@ -26,8 +25,8 @@ const Footer = () => {
                 </Link>
               </div>
               <div className="socials">
-                <ImFacebook className="icon"/>
-                <AiFillInstagram className="icon"/>
+                <ImFacebook className="icon" />
+                <AiFillInstagram className="icon" />
               </div>
             </div>
             <div className="footer_navbar">
@@ -35,15 +34,26 @@ const Footer = () => {
               <ul>
                 {navlinks.map((item) => (
                   <li key={item.id}>
-                    <Link to={item.to}>{item.link}</Link>
+                    <NavLink
+                      to={item.to}
+                      className={({ isActive }) =>
+                        isActive ? "footerR_active" : "footerR"
+                      }
+                    >
+                      {item.link}
+                    </NavLink>
                   </li>
                 ))}
               </ul>
             </div>
             <div className="footer_contacts">
               <h4 className="linkTitle"> Kontaktiere uns </h4>
-              <a href='tel:+ 49 17624310872' className="phone">+ 49 17624310872</a>
-              <a href='mailto:swimmzerotohero@gmail.com' className="email">swimmzerotohero@gmail.com</a>
+              <a href="tel:+ 49 17624310872" className="phone">
+                + 49 17624310872
+              </a>
+              <a href="mailto:swimmzerotohero@gmail.com" className="email">
+                swimmzerotohero@gmail.com
+              </a>
             </div>
 
             <div className="footer_contacts">
@@ -51,24 +61,29 @@ const Footer = () => {
               <p>INI Gesamtschule Bad Sassendorf</p>
               <p>Zur Hepper Höhe 4</p>
               <p>59505 Bad Sassendorf</p>
-              <Map/>
+              <Map />
             </div>
-            
           </div>
-          <div className='footer_rolls'>
+          <div className="footer_rolls">
             <ul>
               {roolsLink.map((item) => (
                 <li key={item.id}>
-                  <Link to={item.to}>{item.link} </Link>
+                  <NavLink
+                    to={item.to}
+                    className={({ isActive }) =>
+                      isActive ? "footerR_active" : "footerR"
+                    }
+                  >
+                    {item.link}
+                  </NavLink>
                 </li>
               ))}
             </ul>
           </div>
-
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -1,58 +1,58 @@
-import React, {useState} from 'react'
-import {AiFillCloseCircle} from 'react-icons/ai'
-import {TbGridDots, TbSwimming} from "react-icons/tb"
-import {Link, NavLink} from 'react-router-dom'
-import {navlinks} from "../utilits/navlinks";
-
+import React, { useState } from "react";
+import { AiFillCloseCircle } from "react-icons/ai";
+import { TbGridDots, TbSwimming } from "react-icons/tb";
+import { Link, NavLink } from "react-router-dom";
+import { navlinks } from "../utilits/navlinks";
 
 const Navbar = () => {
-  const [active, setActive] = useState('navBar')
-
+  const [active, setActive] = useState("navBar");
 
   const showNav = () => {
-    setActive('navBar activeNavbar')
-  }
+    setActive("navBar activeNavbar");
+  };
 
   const removeNav = () => {
-    setActive("navBar")
-  }
-
+    setActive("navBar");
+  };
   return (
-    <section className='navBarSection'>
+    <section className="navBarSection">
       <div className="container">
         <div className="header">
-
           <div className="logoDiv">
-            <Link to='' className='logoIcon'>
-              <TbSwimming className="icon"/>
-              <div className='flexIcon'>
+            <Link to="" className="logoIcon">
+              <TbSwimming className="icon" />
+              <div className="flexIcon">
                 <span>SCHWIMMSCHULE</span>
                 <span>swimming from zero to hero</span>
               </div>
             </Link>
           </div>
           <div className={active}>
-            <ul className='navLists flex'>
-              {
-                navlinks.map(item => (
-                  <li className='navItem' key={item.id}>
-                  <NavLink onClick={removeNav} to={item.to} className='navLink'>{item.link}</NavLink>
-                  </li>
-                ))
-              }
+            <ul className="navLists flex">
+              {navlinks.map((item) => (
+                <li className="navItem" key={item.id}>
+                  <NavLink
+                    to={item.to}
+                    className={({ isActive }) =>
+                      isActive ? "navLink_active" : "navLink"
+                    }
+                  >
+                    {item.link}
+                  </NavLink>
+                </li>
+              ))}
             </ul>
-            <div onClick={removeNav}
-                 className="closeNavbar">
-              <AiFillCloseCircle className='icon'/>
+            <div onClick={removeNav} className="closeNavbar">
+              <AiFillCloseCircle className="icon" />
             </div>
           </div>
           <div onClick={showNav} className="toggleNavbar">
-            <TbGridDots className='icon'/>
+            <TbGridDots className="icon" />
           </div>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
